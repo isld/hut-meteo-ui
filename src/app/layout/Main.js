d9c4e538b454a6db9b21fdf49763c75e09d0d50a
Petar Velkov <Petar_Velkov@epam.com>
2018-01-19 15:17:55 +0200

    Started working on main screen

--- src/app/layout/Main.js
+++ src/app/layout/Main.js
@@ -9,12 +9,22 @@ import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {AppBar, BottomNavigation, BottomNavigationItem, FontIcon, Paper} from "material-ui";
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
 const styles = {
   container: {
-    textAlign: 'center',
-    paddingTop: 200,
+    // textAlign: 'center',
+    // paddingTop: 200,
   },
+  paper: {
+      margin: '2vw',
+      padding: '1em',
+      textAlign: 'center',
+      display: 'inline-block',
+  }
 };
 
 const muiTheme = getMuiTheme({
@@ -23,54 +33,74 @@ const muiTheme = getMuiTheme({
   },
 });
 
+const pageTabs = [
+    {
+      key: '0',
+      label: 'Weather',
+      icon: <FontIcon className="material-icons">wb_sunny</FontIcon>
+    }, {
+      key: '1',
+      label: 'Statistics',
+      icon: <FontIcon className="material-icons">assessment</FontIcon>
+    }, {
+      key: '2',
+      label: 'Camera',
+      icon: <FontIcon className="material-icons">videocam</FontIcon>
+    }, {
+      key: '3',
+      label: 'Map',
+      icon: <FontIcon className="material-icons">terrain</FontIcon>
+    }
+];
+
+
 class Main extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      open: false,
+      drawerOpen: false,
+      selectedIndex: 0
     };
-  }
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
   }
 
   handleTouchTap = () => {
     this.setState({
       open: true,
     });
-  }
+  };
+
+  handleToggle = () => this.setState({drawerOpen: !this.state.open});
+
+  handleClose = () => this.setState({drawerOpen: false});
 
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
-
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
+          <div className="layoutHeader" >
+            <AppBar
+                showMenuIconButton={false}
+                title={"х. Червената шапчица"}
+                onLeftIconButtonClick={this.handleToggle}
+            />
+          </div>
+          <div className="layoutContent">
+            <Paper style={Object.assign({}, styles.paper, {fontSize: '50pt', backgroundColor: 'lightgreen'})}>
+              26&deg;
+            </Paper>
+            <Paper style={Object.assign({}, styles.paper, {fontSize: '50pt', backgroundColor: 'lightblue'})}>
+              44%
+            </Paper>
+          </div>
+          <div className="layoutFooter" style={{position: 'fixed', bottom: 0, width: '100%'}}>
+            <BottomNavigation selectedIndex={this.state.selectedIndex}>
+              {pageTabs.map(function(tab) { return <BottomNavigationItem
+                  {...tab}
+                  onClick={() => this.openPage(tab.key)}
+              />; })}
+            </BottomNavigation>
+          </div>
         </div>
       </MuiThemeProvider>
     );
